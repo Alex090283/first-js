91010e055402a61e769c3b048d44deecaeb9c81d
Alex <alex.dev090283@gmail.com>
2022-12-28 17:27:00 +0200

    lesson 21 finish

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict";
-// alert('Hello')
+
+/* if (4 == 9) {
+    console.log('OK!');
+} else {
+    console.log('Error!!!!');
+} */
+
+/* let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+ */
+
