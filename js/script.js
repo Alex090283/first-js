b1989aeedad1c00a689d76b07b7032774a5f5ecc
Alex <alex.dev090283@gmail.com>
2022-12-29 09:24:51 +0200

    added new code for practics nr 3

--- js/script.js
+++ js/script.js
@@ -16,3 +16,94 @@ if (hamburger && cola || fries === 3 && nuggets) {
 }
  */
 
+// ***************** */ Циклы /******************** */
+
+/* let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+} */
+
+/* let num = 50;
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55); */
+
+/* let num = 50;
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+} */
+
+
+/* for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        break;
+    }
+
+    console.log(i);
+}  */
+
+/* let num = 5;
+
+while (num <= 10 ) {
+    console.log(num);
+    num++;
+}
+
+for (let i = 20; i > 9; i--) {
+    console.log(i);
+    if (i === 14) {
+        break;
+    }
+}
+
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    } else {
+        continue;
+    }
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let i = 2;
+
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+
+const arrayOfNumbers = [];
+
+let ind = 0,
+    num = 5;
+
+while (num < 11) {
+    arrayOfNumbers[ind] = num;
+    ind++;
+    num++;
+}
+
+console.log(arrayOfNumbers); */
+
+
